c885b9d7171f793e769099d519d49efb2eef353a
Nixon Okoth <rgnixonroy@gmail.com>
2022-03-30 15:57:36 +0300

    task 8 correction

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -4,11 +4,12 @@
  * wildcmp - Compare two strings and returns 1 if the strings
  * can be considered identical, otherwise return 0
  * @s1: is a pointer char type that store a string
- * @s2: ia a pointer chat typer that store a string
- * Return: true or false if the s1 and s2 can considerer equal
+ * @s2: ia a pointer chat type that store a string
+ * Return: true or false if the s1 and s2 can considered equal
  * s2 can contain the special character *
  * The special char * can replace any string (including an empty string)
- * function that compares two strings and returns 1 if strings can be considered identical, otherwise return 0.
+ * function compares two strings and returns 1 if strings can be considered identical
+ * otherwise return 0.
  */
 
 int wildcmp(char *s1, char *s2)
