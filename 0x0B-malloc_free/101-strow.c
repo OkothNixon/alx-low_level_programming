657cabf76b0d7ca5d6dc5cb822f92e30f422c737
Nixon Okoth <rgnixonroy@gmail.com>
2022-04-05 02:45:35 +0300

    task 6

--- 0x0B-malloc_free/101-strow.c
+++ 0x0B-malloc_free/101-strow.c
@@ -31,3 +31,50 @@ char **strtow(char *str)
 
 	return (spll(str, cont1));
 }
+
+/**
+  * spll - splits a string into words
+  * @str: a string with spaces
+  * @cont1: is a counter
+  * Return: an address
+  */
+
+char **spll(char *str, int cont1)
+{
+	int i, j, k, cont2, flag1 = 0;
+	char **splitt;
+
+	splitt = malloc((cont1 + 1) * sizeof(char *));
+	if (!splitt)
+		return (NULL);
+	for (i = 0, j = 0, flag1 = 0, cont2 = 0; i < cont1; i++, j++)
+	{
+		for ( ; str[j] != '\0'; j++)
+		{
+			if (str[j] != ' ')
+			{
+				cont2++;
+				flag1 = 1;
+			}
+			if (flag1 == 1 && (str[j + 1] == ' ' || str[j + 1] == '\0'))
+			{
+				flag1 = 0;
+				splitt[i] = malloc(cont2 * sizeof(char) + 1);
+				if (splitt[i] == NULL)
+				{
+					for (i--; i >= 0; i--)
+						free(splitt[i]);
+					free(splitt);
+					return (NULL);
+				}
+				for (k = 0; k < cont2; k++)
+					splitt[i][k] = str[(j - cont2 + 1) + k];
+				splitt[i][k] = '\0';
+				cont2 = 0;
+				break;
+			}
+		}
+	}
+	splitt[i] = '\0';
+	return (splitt);
+}
