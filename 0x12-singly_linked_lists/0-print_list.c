c6bdfa9134cb089bb82a8c39ad4eed7b409db929
Nixon Okoth <rgnixonroy@gmail.com>
2022-04-21 14:53:39 +0300

    task 0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -8,20 +8,20 @@
 
 size_t print_list(const list_t *h)
 {
-    size_t I;
+	size_t i;
 
-    for (i = 0; h != NULL; i++)
-    {
-        if (h->str != NULL)
-        {
-            printf("[%d] %s\n", h->len, h->str);
-            h = h->next;
-        }
-        else
-        {
-            printf("[%d] %s\n", 0, "(nil)");
-            h = h->next;
-        }
-    }
-    return (i);
+	for (i = 0; h != NULL; i++)
+	{
+		if (h->str != NULL)
+		{
+			printf("[%d] %s\n", h->len, h->str);
+			h = h->next;
+		}
+		else
+		{
+			printf("[%d] %s\n", 0, "(nil)");
+			h = h->next;
+		}
+	}
+	return (i);
 }
